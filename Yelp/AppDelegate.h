0eb47b684a3f746c9b50c2c09305e6f5197c5046
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-03-20 19:26:35 -0700

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Stephanie Szeto on 3/20/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Stephanie Szeto on 3/20/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
